4eedfa422fb091692d536b34c5920f77cc22944f
GabbsDutra <gakillglikerr@gmail.com>
2024-09-06 18:51:29 -0400

    parece feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import "./globals.css";
 export default function App() {
 
-  const nome = "João";
-  const sobreNome = "Santos";
+  
 
   return(
     <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
+        <Header/>
+        <Footer/>
     </div>  
   );
 
