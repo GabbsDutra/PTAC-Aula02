8d95ff2b0c3b25d6c90fe9d27429e7ed9f728af4
GabbsDutra <gakillglikerr@gmail.com>
2024-09-06 18:04:04 -0400

    mudei

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,8 @@ export default function App() {
 
   return(
     <div>
-     <Header/>
-     <Footer/>
-=======
-import Header from "./Components/Header";
-import Footer from "./Components/Footer"
-export default function App() {
-  return(
-    <div>
-       <Header/>
-       <Footer/>
->>>>>>> 5b00eb0343f110971accaeaa04dd0b21ce9e1d1b
-    </div>  
-  );
+      <Header/>
+      <Footer/>
+      </div>
+  );}
 
-
-}
