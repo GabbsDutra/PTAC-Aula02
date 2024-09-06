685e251cc57c1e34b7213143df83ec7877bc20bc
GabbsDutra <gakillglikerr@gmail.com>
2024-09-06 19:01:19 -0400

    muda

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import "./globals.css";
-
-
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
-
 export default function App() {
 
   
 
   return(
     <div>
-
-        <Header/>
-        <Footer/>
-    </div>  
-  );
-
-}
-
+      <Header/>
+      <Footer/>
+      </div>
+  );}
