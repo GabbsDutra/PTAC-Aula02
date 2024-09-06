ac281bfc47e4bf94acd6a7449c96d57d1e321b0d
GabbsDutra <gakillglikerr@gmail.com>
2024-09-06 18:53:48 -0400

    pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
+
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import "./globals.css";
-=======
+
 
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
->>>>>>> 8d95ff2b0c3b25d6c90fe9d27429e7ed9f728af4
+
 export default function App() {
 
   
 
   return(
     <div>
-<<<<<<< HEAD
+
         <Header/>
         <Footer/>
     </div>  
   );
-=======
-      <Header/>
-      <Footer/>
-      </div>
-  );}
->>>>>>> 8d95ff2b0c3b25d6c90fe9d27429e7ed9f728af4
+
+}
 
